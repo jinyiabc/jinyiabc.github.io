0b727f2acc977508761512e04637250ec265fac8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-05-07 06:28:18 +0000

    Deploy jinyiabc/jinyiabc.github.io to github.com/jinyiabc/jinyiabc.github.io.git:master

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-2603b60ba683836931ea.js","227eb2869d9a95b28cdb825e6888afdc"],["/commons-be44d5a1eb85dcd3d233.js","300a5ae2524aa1cdd325ade6f74d8b77"],["/index.html","1c476033c9863a5af4ada12e4d93da46"],["/offline-plugin-app-shell-fallback/index.html","a46baea85fdaa70a553ba950bc8a9e88"]];
+var precacheConfig = [["/app-2603b60ba683836931ea.js","227eb2869d9a95b28cdb825e6888afdc"],["/commons-be44d5a1eb85dcd3d233.js","300a5ae2524aa1cdd325ade6f74d8b77"],["/index.html","bd19bab0654277c81d3d3f27a09594be"],["/offline-plugin-app-shell-fallback/index.html","113fc170f120867e22764965080a96d0"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
